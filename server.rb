a546bb799cdae2550b5db56fd816f4fe878aadb0
Chris Wood <chriswood.ca@gmail.com>
2014-06-11 11:31:38 -0700

    Client and Server file skeletons Doc header and empty skeleton

---
+++ server.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+=begin
+-------------------------------------------------------------------------------------
+-- SOURCE FILE:   server.rb - description goes here
+--
+-- PROGRAM:       server
+--                ./server.rb
+--
+-- FUNCTIONS:     stuff here
+--
+-- Ruby Gems req: 
+-- 
+-- DATE:          May/June 2014
+--
+-- REVISIONS:     See development repo: https://github.com/deuterium/comp8505-final
+--
+-- DESIGNERS:     Chris Wood - chriswood.ca@gmail.com
+--
+-- PROGRAMMERS:   Chris Wood - chriswood.ca@gmail.com
+-- 
+-- NOTES:         notes might go here
+---------------------------------------------------------------------------------------
+=end
+
+require 'packetfu'
+
+## Variables
+
+## Functions
+
+## Main
