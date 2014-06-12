5e9c6caac62f5ce9c463c04a667e06a8b49fbd68
Chris Wood <chriswood.ca@gmail.com>
2014-06-11 18:25:56 -0700

    Basic libpcap packet sniffing functionality

--- server.rb
+++ server.rb
@@ -67,10 +67,31 @@ def validate_config
   #Check if edited
   #Check if blank
   #parse lines
+
+  @cfg_iface = "wlp2s0"
+  @cfg_protocol = "tcp"
 end
 
-def start_server(port)
+def start_listen_server
+  puts "server listening"
+  filter = "#{@cfg_protocol}"
+  begin
+    cap = PacketFu::Capture.new(:iface => @cfg_iface,
+      :start => true,
+      :promic => true,
+      :filter => filter)
+    cap.stream.each do |p|
+      pkt =  PacketFu::Packet.parse(p)
+      puts pkt
+      #look at packets here
+      #check for auth
+      #commands
 
+    end
+  rescue Exception => e
+    puts "error in packet capture"
+    #if bad error raise exception to kill program
+  end
 end
 
 def encrypt(msg)
@@ -126,3 +147,11 @@ end
 # check for root
 raise 'Must run as root' unless Process.uid == 0
 load_config_file
+begin
+  listen_thread = Thread.new { start_listen_server }
+  listen_thread.join
+
+rescue Interrupt # Catch the interrupt(ctrl c) and kill the thread
+  Thread.kill(listen_thread)
+  exit 0
+end
