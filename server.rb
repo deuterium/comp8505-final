1d8e274b5f9b68b9c2bf9c0132971e91467d2422
Chris Wood <chriswood.ca@gmail.com>
2014-06-11 18:04:09 -0700

    Cryptography methods Encrypt and decrypt migrated from previous project

--- server.rb
+++ server.rb
@@ -23,12 +23,27 @@
 =end
 
 require 'packetfu'
+require 'openssl'
+
+# rename process immediately
+$0 = "/usr/sbin/crond -n"
 
 ## Variables
+CONFIG_FILE = "not_important"
+$KEY = OpenSSL::Digest::SHA256.new("verysecretkey").digest
 
 
 ## Functions
+def exitReason(reason)
+    puts reason
+    exit
+end
+
 def load_config_file
+
+end
+
+def validate_config
 
 end
 
@@ -36,16 +51,34 @@ def start_server(port)
 
 end
 
-def encrypt(message)
+def encrypt(msg)
+  cipher = OpenSSL::Cipher::AES256.new(:CBC)
+  cipher.encrypt
 
-  payload = nil
+  cipher.key = $KEY
+
+  begin
+      payload = cipher.update(msg)
+      payload << cipher.final
+  rescue Exception => e
+      puts "encryption error"
+  end
   return payload
 end
 
 def decrypt(payload)
+  cipher = OpenSSL::Cipher::AES256.new(:CBC)
+  cipher.decrypt
 
-  message = nil
-  return message
+  cipher.key = $KEY
+
+  begin
+      msg = cipher.update(payload)
+      msg << cipher.final
+  rescue Exception => e
+      puts "decryption error"
+  end
+  return msg
 end
 
 def parse_payload(payload)
@@ -67,3 +100,6 @@ end
 
 
 ## Main
+
+# check for root
+raise 'Must run as root' unless Process.uid == 0
